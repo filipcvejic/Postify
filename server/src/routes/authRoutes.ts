bda354d2a290816010d085b7869f23ca672ec824
Filip Cvejic <97543155+filipcvejic@users.noreply.github.com>
2024-08-08 23:56:17 +0200

    Created auth login and register routes

---
+++ server/src/routes/authRoutes.ts
@@ -0,0 +1,60 @@
+import express from "express";
+import {
+  login,
+  logout,
+  refresh,
+  register,
+} from "../controllers/authController";
+import validateRequest from "../middleware/validateRequest";
+import { body } from "express-validator";
+
+const router = express.Router();
+
+router.post(
+  "/",
+  body("email")
+    .not()
+    .isEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Please enter a valid email"),
+  body("password")
+    .not()
+    .isEmpty()
+    .withMessage("Password is required")
+    .matches(/^(?=.*?[0-9])(?=.*?[A-Z])(?=.*?[a-z]).{6,32}$/)
+    .withMessage(
+      "Password must be between 6 and 32 characters, and include at least one uppercase letter, one lowercase letter, and one number."
+    ),
+  validateRequest,
+
+  login
+);
+router.get("/refresh", refresh);
+router.post("/logout", logout);
+router.post(
+  "/register",
+  body("username")
+    .not()
+    .isEmpty()
+    .withMessage("Username is required")
+    .isLength({ min: 3 }),
+  body("email")
+    .not()
+    .isEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Please enter a valid email"),
+  body("password")
+    .not()
+    .isEmpty()
+    .withMessage("Password is required")
+    .matches(/^(?=.*?[0-9])(?=.*?[A-Z])(?=.*?[a-z]).{6,32}$/)
+    .withMessage(
+      "Password must be between 6 and 32 characters, and include at least one uppercase letter, one lowercase letter, and one number."
+    ),
+  validateRequest,
+  register
+);
+
+export default router;
--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,59 +1,10 @@
 import { Router } from "express";
 import {
   acceptFriendRequest,
-  login,
-  register,
   sendFriendRequest,
-} from "../controllers/userController.js";
-import { body } from "express-validator";
-import validateRequest from "../middlewares/validateRequest.js";
-import { acceptFriend } from "../models/friendModel.js";
+} from "../controllers/userController";
 
 const router = Router();
-
-router.post(
-  "/auth/register",
-  body("username")
-    .not()
-    .isEmpty()
-    .withMessage("Username is required")
-    .isLength({ min: 3 }),
-  body("email")
-    .not()
-    .isEmpty()
-    .withMessage("Email is required")
-    .isEmail()
-    .withMessage("Please enter a valid email"),
-  body("password")
-    .not()
-    .isEmpty()
-    .withMessage("Password is required")
-    .matches(/^(?=.*?[0-9])(?=.*?[A-Z])(?=.*?[a-z]).{6,32}$/)
-    .withMessage(
-      "Password must be between 6 and 32 characters, and include at least one uppercase letter, one lowercase letter, and one number."
-    ),
-  validateRequest,
-  register
-);
-router.post(
-  "/auth/login",
-  body("email")
-    .not()
-    .isEmpty()
-    .withMessage("Email is required")
-    .isEmail()
-    .withMessage("Please enter a valid email"),
-  body("password")
-    .not()
-    .isEmpty()
-    .withMessage("Password is required")
-    .matches(/^(?=.*?[0-9])(?=.*?[A-Z])(?=.*?[a-z]).{6,32}$/)
-    .withMessage(
-      "Password must be between 6 and 32 characters, and include at least one uppercase letter, one lowercase letter, and one number."
-    ),
-  validateRequest,
-  login
-);
 
 router.post("/accept-request/:friendId", acceptFriendRequest);
 router.post("/send-request/:friendId", sendFriendRequest);
