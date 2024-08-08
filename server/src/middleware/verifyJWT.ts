1a68384ae9aa22c500a945bc1511e1baa3c888d8
Filip Cvejic <97543155+filipcvejic@users.noreply.github.com>
2024-08-08 23:56:33 +0200

    Implemented jwt middleware

---
+++ server/src/middleware/verifyJWT.ts
@@ -0,0 +1,28 @@
+import { RequestHandler } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { throwError } from "../helpers/throwError";
+
+export const verifyJWT: RequestHandler = (req, res, next) => {
+  const authHeader = req.headers.authorization || req.headers.Authorization;
+
+  if (typeof authHeader !== "string" || !authHeader.startsWith("Bearer ")) {
+    return throwError("Unauthorized", 401);
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(
+    token,
+    process.env.REFRESH_TOKEN_SECRET!,
+    async (err: Error | null, decoded: string | JwtPayload | undefined) => {
+      if (err) return throwError("Forbidden", 403);
+
+      if (decoded && typeof decoded !== "string") {
+        req.user = decoded.UserInfo;
+        next();
+      } else {
+        return throwError("Invalid token", 400);
+      }
+    }
+  );
+};
