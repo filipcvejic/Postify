e848c6a00edd6b258eafa18d8772e689813f6799
Filip Cvejic <97543155+filipcvejic@users.noreply.github.com>
2024-08-11 23:46:48 +0200

    Added jwt middleware to user action routes

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -3,8 +3,11 @@ import {
   acceptFriendRequest,
   sendFriendRequest,
 } from "../controllers/userController";
+import { verifyJWT } from "../middleware/verifyJWT";
 
 const router = Router();
+
+router.use(verifyJWT);
 
 router.post("/accept-request/:friendId", acceptFriendRequest);
 router.post("/send-request/:friendId", sendFriendRequest);
