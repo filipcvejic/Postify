a005051bacf6f0eed3da5dd82856b6338756bcba
Filip Cvejic <97543155+filipcvejic@users.noreply.github.com>
2024-08-26 18:48:56 +0200

    Created user interface and implemented wherever it's necessary

--- server/src/models/userModel.ts
+++ server/src/models/userModel.ts
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import { IUser } from "../interfaces/IUser";
 
 const userSchema = new Schema({
   username: {
@@ -21,8 +22,8 @@ export const User = mongoose.model("User", userSchema);
 export const getUsers = () => User.find().lean();
 export const getUserByEmail = (email: string) => User.findOne({ email }).lean();
 export const getUserById = (id: string) => User.findById(id).lean();
-export const createUser = (values: Record<string, any>) =>
+export const createUser = (values: IUser) =>
   new User(values).save().then((user) => user.toObject());
 export const deleteUserById = (id: string) => User.findByIdAndDelete(id);
-export const updateUserById = (id: string, values: Record<string, any>) =>
+export const updateUserById = (id: string, values: IUser) =>
   User.findByIdAndUpdate(id, values);
