e2575d8b63b2e2cbe5b28d30cfb3657e66728511
Filip Cvejic <97543155+filipcvejic@users.noreply.github.com>
2024-08-26 18:50:13 +0200

    Added axios api and api error handler

---
+++ client/src/api/postifyApi.ts
@@ -0,0 +1,45 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+import { getCookie } from "react-use-cookie";
+
+export const postifyApi = axios.create({
+  baseURL: "http://localhost:5000",
+  headers: {
+    Authorization: `Bearer ${getCookie("jwt")}`,
+  },
+});
+
+export const handleApiError = (error: any) => {
+  switch (error.response.status) {
+    case 400:
+      if (!error.response.data) {
+        toast.error("Greška 400");
+        return;
+      }
+
+      if (Array.isArray(error.response.data)) {
+        error.response.data.forEach(
+          (item: any) => item.ErrorMessage && toast.error(item.ErrorMessage)
+        );
+        return;
+      }
+
+      toast.error(error.response.data);
+      return;
+    case 401:
+      toast.error("Niste autentikovani");
+      return;
+    case 403:
+      toast.error("Nemate pravo pristupa");
+      return;
+    case 404:
+      toast.error("Nije pronađeno");
+      return;
+    case 500:
+      toast.error("Greška na serveru");
+      return;
+    default:
+      toast.error("Greška");
+      return;
+  }
+};
