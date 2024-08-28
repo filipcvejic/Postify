41425445f0fb32d95a75dbd00f6d9711edd8d312
Filip Cvejic <97543155+filipcvejic@users.noreply.github.com>
2024-08-28 23:59:45 +0200

    Clean up console logs

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -4,7 +4,6 @@ export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI!);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
-    console.log(process.env.ACCESS_TOKEN_SECRET);
   } catch (err) {
     console.error(`Error: ${err}`);
     process.exit(1);
