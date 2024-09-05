cf83d4763219fa992b0da94f30131f7c38249d60
Filip Cvejic <97543155+filipcvejic@users.noreply.github.com>
2024-09-05 23:36:27 +0200

    Removed try/catch and added asyncHandler to comment controller handlers

--- server/src/controllers/commentController.ts
+++ server/src/controllers/commentController.ts
@@ -2,40 +2,38 @@ import { Request, Response, NextFunction } from "express";
 import { throwError } from "../helpers/throwError";
 import { IComment } from "../interfaces/IComment";
 import { createComment, editCommentById } from "../models/commentModel";
+import asyncHandler from "express-async-handler";
 
-export const addComment = async (
-  req: Request<{}, {}, IComment>,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
+export const addComment = asyncHandler(
+  async (req: Request<{}, {}, IComment>, res: Response, next: NextFunction) => {
     const { postId, parentId, userId, text } = req.body;
 
     if (!postId || !userId || !text) {
       return throwError("Post ID, User ID and Text are required.", 400);
     }
 
-    const newComment = await createComment({ postId, parentId, userId, text });
+    const newComment = await createComment({
+      postId,
+      parentId,
+      userId,
+      text,
+    });
 
     res.status(200).json(newComment);
-  } catch (err) {
-    next(err);
   }
-};
+);
 
-export const editComment = async (
-  req: Request<{ commentId: string }, {}, IComment>,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
+export const editComment = asyncHandler(
+  async (
+    req: Request<{ commentId: string }, {}, IComment>,
+    res: Response,
+    next: NextFunction
+  ) => {
     const { commentId } = req.params;
     const updateValues = req.body;
 
     const updatedComment = await editCommentById(commentId, updateValues);
 
     res.status(200).json(updatedComment);
-  } catch (err) {
-    next(err);
   }
-};
+);
