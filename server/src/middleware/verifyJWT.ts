871e51bfa021596fd956457e1b3fce7033639644
Filip Cvejic <97543155+filipcvejic@users.noreply.github.com>
2024-09-06 23:52:24 +0200

    Made some jwt middleware approach changes

--- server/src/middleware/verifyJWT.ts
+++ server/src/middleware/verifyJWT.ts
@@ -1,28 +1,24 @@
 import { RequestHandler } from "express";
-import jwt, { JwtPayload } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import { throwError } from "../helpers/throwError";
 
 export const verifyJWT: RequestHandler = (req, res, next) => {
-  const authHeader = req.headers.authorization || req.headers.Authorization;
+  const token = req.cookies.jwt;
 
-  if (typeof authHeader !== "string" || !authHeader.startsWith("Bearer ")) {
+  if (!token) {
     return throwError("Unauthorized", 401);
   }
 
-  const token = authHeader.split(" ")[1];
+  try {
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
-  jwt.verify(
-    token,
-    process.env.ACCESS_TOKEN_SECRET,
-    async (err: Error | null, decoded: string | JwtPayload | undefined) => {
-      if (err) return throwError("Forbidden", 403);
-
-      if (decoded && typeof decoded !== "string") {
-        req.user = decoded.UserInfo;
-        next();
-      } else {
-        return throwError("Invalid token", 400);
-      }
+    if (decoded && typeof decoded !== "string") {
+      req.user = decoded.UserInfo;
+      next();
+    } else {
+      return throwError("Invalid token", 400);
     }
-  );
+  } catch (err) {
+    return next(err);
+  }
 };
