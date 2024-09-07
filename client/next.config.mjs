fcf207aa3097059d3a366276f36fa79bd4beae81
Filip Cvejic <97543155+filipcvejic@users.noreply.github.com>
2024-09-07 23:46:14 +0200

    Created new client folder with app router

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["postify.s3.eu-central-1.amazonaws.com"],
+  },
+};
 
 export default nextConfig;
