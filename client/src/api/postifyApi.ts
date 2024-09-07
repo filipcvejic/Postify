e96ae3f9c385b83827fad78c83446920a3e03520
Filip Cvejic <97543155+filipcvejic@users.noreply.github.com>
2024-09-07 23:47:59 +0200

    Fixed typescript axios error handler problem

--- client/src/api/postifyApi.ts
+++ client/src/api/postifyApi.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { toast } from "react-toastify";
 
 export const postifyApi = axios.create({
@@ -6,38 +6,46 @@ export const postifyApi = axios.create({
   withCredentials: true,
 });
 
-export const handleApiError = (error: any) => {
-  console.log(error.response.data.message);
-  switch (error.response.status) {
-    case 400:
-      if (!error.response.data) {
-        toast.error("Greška 400");
-        return;
-      }
+interface ErrorResponse {
+  status: number;
+  data: string | { message?: string } | Array<{ ErrorMessage?: string }>;
+}
 
-      if (Array.isArray(error.response.data)) {
-        error.response.data.forEach(
-          (item: any) => item.ErrorMessage && toast.error(item.ErrorMessage)
-        );
-        return;
-      }
+export const handleApiError = (error: AxiosError<ErrorResponse>) => {
+  if (error.response) {
+    const { status, data } = error.response;
 
-      toast.error(error.response.data.message);
-      return;
-    case 401:
-      toast.error("Niste autentikovani");
-      return;
-    case 403:
-      toast.error("Nemate pravo pristupa");
-      return;
-    case 404:
-      toast.error("Nije pronađeno");
-      return;
-    case 500:
-      toast.error("Greška na serveru");
-      return;
-    default:
-      toast.error("Greška");
-      return;
+    switch (status) {
+      case 400:
+        if (typeof data === "string") {
+          toast.error(data);
+        } else if (Array.isArray(data)) {
+          data.forEach((item) => {
+            if (item.ErrorMessage) {
+              toast.error(item.ErrorMessage);
+            }
+          });
+        } else {
+          toast.error("Greška 400");
+        }
+        return;
+      case 401:
+        toast.error("Niste autentikovani");
+        return;
+      case 403:
+        toast.error("Nemate pravo pristupa");
+        return;
+      case 404:
+        toast.error("Nije pronađeno");
+        return;
+      case 500:
+        toast.error("Greška na serveru");
+        return;
+      default:
+        toast.error("Greška");
+        return;
+    }
+  } else {
+    toast.error("Greška u komunikaciji sa serverom");
   }
 };
